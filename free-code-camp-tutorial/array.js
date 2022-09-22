fe099b181be6ffdc9895e01f4c32afb254e3f0b3
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-22 16:41:40 -0400

    arrays

---
+++ free-code-camp-tutorial/array.js
@@ -0,0 +1,14 @@
+// Store Multiple Values in one Variable using JavaScript Arrays
+//Modify the new array myArray so that it contains both a string and a number (in that order).
+const myArray = ["string", 1]
+console.log(myArray);
+
+// Nest one Array within Another Array
+const newArray = [['apples', 5], ['Oranges', 3]];
+console.log(newArray);
+
+// Access Array Data with Indexes
+const numArray = [50, 60, 70];
+console.log(numArray[0]);
+const data = numArray[1];
+console.log(data);
