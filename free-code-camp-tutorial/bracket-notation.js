6db46dbd258b9e05fe785885bba823054bad799b
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-22 15:48:58 -0400

    bracket-notation

---
+++ free-code-camp-tutorial/bracket-notation.js
@@ -0,0 +1,24 @@
+// Use Bracket Notation to Find the Nth Character in a String
+// eg. const firstName = "Ada";
+// const secondLetterOfFirstName = firstName[1];
+
+// secondLetterOfLastName would have a value of the string d. 
+
+// Setup
+// Third letter of the last name
+const lastName = 'LoveLace';
+const thirdLetterOfLastName = lastName[2];
+
+console.log(thirdLetterOfLastName);
+
+// To find the last character in a string
+
+const lastCharacterOfString = lastName[lastName.length - 1];
+
+console.log(lastCharacterOfString);
+
+// Use Bracket Notation to Find the second-to-Last Character in a String
+
+const secondToLastCharacter = lastName[lastName.length - 2];
+console.log(secondToLastCharacter);
+
