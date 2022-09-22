e0570c6353cb47e70d8dda19c3f9af401bfed33e
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-22 11:03:38 -0400

    practice day-one

---
+++ free-code-camp-tutorial/first-character.js
@@ -0,0 +1,21 @@
+//Use Bracket Notation to Find the First Character in a String
+
+
+// const firstName = "Charles";
+// const firstLetter = firstName[0];
+
+// console.log(firstLetter); // returns "C";
+
+//Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+// Setup
+let firstLetterOfLastName = "";
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; //Change this line
+
+
+
+
---
+++ free-code-camp-tutorial/string-immutability.js
@@ -0,0 +1,17 @@
+//In JavaScript, String values are immutable, which means that they cannot be altered once created.
+
+// let myStr = "Bob";
+
+// myStr[0] = "J";
+
+// Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+// Setup 
+
+let myStr = "Jello World";
+
+myStr = "Hello World";
+
+console.log(myStr);
+
+// changing the letter "J" in myString variable wasn't possible due to string values being immutable so the whole myStr was assigned a new value.
