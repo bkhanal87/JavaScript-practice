d8ed644dc1d0014c246c21058e149cd848613520
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-25 23:12:41 -0400

    convert if/else statement to switch statement

---
+++ free-code-camp-tutorial/js-part2/replace-ifelse-with-switch.js
@@ -0,0 +1,30 @@
+// Replacing If Else Chains with Switch
+// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements.
+
+// Change the chained if/else if statements into a switch statement.
+
+
+function chainToSwitch(val) {
+    let answer = "";
+
+    switch(val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99: 
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+    }
+
+    return answer;
+}
+
+chainToSwitch(7);
