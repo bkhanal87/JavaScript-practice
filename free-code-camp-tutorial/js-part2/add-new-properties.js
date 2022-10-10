86c5ab81fba77995dbe34bcee64d645ab6192780
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-10-09 23:21:09 -0400

    add new properties to js obj

---
+++ free-code-camp-tutorial/js-part2/add-new-properties.js
@@ -0,0 +1,14 @@
+// You can add new properties to existing JavaScript objects the same way you would modify them.
+
+// Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Chase", "Bruno", "Emily"]
+};
+
+myDog.sound = "woof";
+
+console.log(myDog) // sound property with a value of "woof" added
